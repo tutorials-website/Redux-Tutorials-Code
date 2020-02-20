4b8be3e39a5e7723b6489df089d04edd26a85450
Tutorials Website <46397286+tutorials-website@users.noreply.github.com>
2020-02-20 23:28:20 +0530

    update getting started

--- getting-started.js
+++ getting-started.js
@@ -1,1 +1,38 @@
 
+const redux =require('redux');
+const createStore=redux.createStore;
+
+const intialState={
+    numberOfBooks:10,
+}
+
+// action creator: wraping the action in a single function
+function buyBook(){
+    return {
+        type:"Buy_Book",
+        payload:"My First Action"
+    }
+}
+
+// (prevState,action)=>newState
+
+const Reducer =(state=intialState,action)=>{
+    switch(action.type){
+        case "Buy_Book":return{
+            ...state,
+            numberOfBooks:state.numberOfBooks-1
+        }
+
+
+        default: return state;
+    }
+}
+
+const store =createStore(Reducer);
+console.log("Initial State",store.getState());
+const unsubscribe=store.subscribe(()=>{console.log('Updated State Value', store.getState())});
+store.dispatch(buyBook());
+store.dispatch(buyBook());
+store.dispatch(buyBook());
+store.dispatch(buyBook());
+unsubscribe();
